38202a799e86b3ff9c84052c4b7cc8b1a5b382a7
Declan Glover <declan2317@hotmail.com>
2020-09-15 21:29:38 +1000

    Additional information added to API reponse, time, humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request = require('request')
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=94c48b2e8ccd53bfc824605675a3c35b&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -8,9 +10,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] +
-                '. It is currently ' + body.current.temperature + ' degrees out,' +
-                ' feels like ' + body.current.feelslike + ' degrees')
+            callback(undefined, 'Conditions:' +
+                body.current.weather_descriptions[0] +
+                ', It is currently ' + body.current.temperature + ' degrees out,' +
+                ' feels like: ' + body.current.feelslike + ', Humidity: ' + body.current.humidity +
+                ', Time: ' + body.current.observation_time)
         }
 
     })
